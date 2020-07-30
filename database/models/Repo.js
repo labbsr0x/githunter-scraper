58d7f84911f0e3c6e15c468533d51ecec7c48881
Rafael Farias Lima <finha.farias@outlook.com>
2020-07-30 14:20:47 -0300

    add new attributes on repo model for support data in code page

--- database/models/Repo.js
+++ database/models/Repo.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const language = new Schema({
+  name: {
+    type: String
+  },
+  percent: {
+    type: String
+  }
+});
+
 const repoSchema = new Schema({
   name: {
     type: String
@@ -11,9 +20,40 @@ const repoSchema = new Schema({
   license: {
     type: String
   },
+  readme: {
+    type: String
+  },
+  watchers: {
+    type: Number
+  },
+  stars: {
+    type: Number
+  },
+  forks: {
+    type: Number
+  },
+  defaultBranch: {
+    type: String,
+  },
+  commitsQuantity: {
+    type: Number,
+  },
+  lastCommitTime: {
+    type: Date
+  },
   contributorsQuantity: {
     type: Number
-  }
+  },
+  url: {
+    type: String
+  },
+  releases: {
+    type: Number
+  },
+  topics: [String],
+  languages: [language]
+  //commits
+  //issues
 }, {timestamps: true});
 
 module.exports = mongoose.model('repoCollection', repoSchema);
